ce7b1bad24f4db9c5a2c199aa90ad27f39b3a4a1
kathhulasampathkumar <92053397+kathhulasampathkumar@users.noreply.github.com>
2023-10-20 06:24:09 +0000

    2nd commit 20 10 2023

--- service/src/main/java/com/example/service/ServiceApplication.java
+++ service/src/main/java/com/example/service/ServiceApplication.java
@@ -1,7 +1,13 @@
 package com.example.service;
 
+import java.util.Collection;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ServiceApplication {
@@ -10,5 +16,23 @@ public class ServiceApplication {
 		SpringApplication.run(ServiceApplication.class, args);
 		System.out.println("Service Spring Boot started");
 	}
+
+}
+class CustomerHttpController{
+	private final CustomerRepository repository;
+	CustomerHttpController(CustomerRepository repository){
+		this.repository=repository;
+	}
+	@GetMapping("/cust")
+	Collection<Customer> customers(){
+		return this.repository.findAll();
+	}
+}
+
+interface CustomerRepository extends ListCrudRepository<Customer, Integer>{
+
+}
+
+record Customer(@Id Integer id, String name){
 
 }
---
+++ service/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Customers (name) VALUES
+    ('Customer 1'),
+    ('Customer 2'),
+    ('Customer 3'),
+    ('Customer 4'),
+    ('Customer 5'),
+    ('Customer 6'),
+    ('Customer 7'),
+    ('Customer 8'),
+    ('Customer 9'),
+    ('Customer 10');
